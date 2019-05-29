c5b542da48fe2f509941a50f9396a75750afde01
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-29 17:12:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.find_all do |word|
+      a = word.split("").sort
+      b = @word.split("").sort
+      a == b
+    end
+  end
+
+end
